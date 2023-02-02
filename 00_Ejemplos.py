03d062561f668cf5b4905fe1285c03272d3c595c
guilleromera <124285353+guilleromera@users.noreply.github.com>
2023-02-02 17:47:50 +0100

    Add files via upload

---
+++ 00_Ejemplos.py
@@ -0,0 +1,14 @@
+print ('Hola Guille este es tu primer programa')
+
+
+print(type("Soy un dato str"))  # Tipo 'str'
+print(type(5))  # Tipo 'int'
+print(type(1.5))  # Tipo 'float'
+print(type(3 + 1j))  # Tipo 'complex'
+print(type(True))  # Tipo 'bool'
+print(type(print("Mi cadena de texto")))  # Tipo 'NoneType'
+
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
